851436dc48cb6744d74f6854d8c7c463745a93d8
Aydin Baran Ertemir <64380389+baranertemir@users.noreply.github.com>
2020-09-19 13:55:42 +0300

    Add files via upload

---
+++ yasak_kaldir.py
@@ -0,0 +1,13 @@
+@client.command()
+@commands.has_permissions(administrator = True)
+async def bankaldir(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split("#")
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Bani acildi {user.mention}')
+            return
