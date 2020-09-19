4966ed3d9d2d6ea20a30f90ef6c24b3a9e082db1
Aydin Baran Ertemir <64380389+baranertemir@users.noreply.github.com>
2020-09-19 13:39:49 +0300

    Delete rol_ekle.py

--- rol_ekle.py
+++
@@ -1,5 +0,0 @@
-@client.command(pass_context=True)
-@commands.has_permissions(manage_roles=True)
-async def rolekle(ctx, user: discord.Member, role: discord.Role):
-    await user.add_roles(role)
-    await ctx.send(f"Kral {ctx.author.name}, {user.name} denen bu türemeye bir rol verdi: {role.name}")
